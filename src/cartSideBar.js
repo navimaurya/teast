2aa17252d30299def53f554539230359ea91d680
navimaurya <mauryanavis@gmail.com>
2021-12-12 17:11:25 +0530

    fupdate

--- src/cartSideBar.js
+++ src/cartSideBar.js
@@ -15,20 +15,29 @@ const CartSidebar = ({ cartItems }) => {
             </div>
             <hr />
             {
-                cartItems.map((item, i) => {
-                    total += item.quantity * item.price;
-                    totalItems += item.quantity
-                    return (
-                        <CartItem item={item} key={i} />
-                    )
-                })
+                cartItems.length ? <>
+
+                    {
+                        cartItems.map((item, i) => {
+                            total += item.quantity * item.price;
+                            totalItems += item.quantity
+                            return (
+                                <CartItem item={item} key={i} />
+                            )
+                        })
+                    }
+                    <hr />
+                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <h5 className='py-2'>Item Total <sub>({totalItems})</sub></h5>
+                        <p>${total}</p>
+                    </div>
+                    <Link to='/cart' className='btn btn-warning w-100 py-2 text-white'>Check Out</Link>
+                </>
+                    : <div className='w-100 d-flex text-center' style={{ justifyContent: 'center', minHeight: '60vh', flexDirection: 'column', backgroundImage: 'src(https://www.pngplay.com/wp-content/uploads/7/Cart-Transparent-Free-PNG.png)' }}>
+                        <img src='https://www.pngplay.com/wp-content/uploads/7/Cart-Transparent-Free-PNG.png' alt='...' />
+                        <h3 className='display-6'>Your Cart is Empty</h3>
+                    </div>
             }
-            <hr />
-            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                <h5 className='py-2'>Item Total <sub>({totalItems})</sub></h5>
-                <p>${total}</p>
-            </div>
-            <Link to='/cart' className='btn btn-warning w-100 py-2 text-white'>Check Out</Link>
         </div>
     )
 }
